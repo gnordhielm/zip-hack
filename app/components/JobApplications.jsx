2bf2a1e5fa040658418ca24c6f7b49f3a60b451f
Gus Nordhielm <gnordhielm@gmail.com>
2017-05-07 14:13:04 -0700

    add dummy presentation features

--- app/components/JobApplications.jsx
+++ app/components/JobApplications.jsx
@@ -6,23 +6,80 @@ import NewApplication from 'NewApplication'
 import JobApplication from 'JobApplication'
 
 export var JobApplications = React.createClass({
+	getInitialState: function() {
+		return {
+			applications:[{
+				id: 1,
+				company: 'Hulu',
+				title: 'Software Developer',
+				location: 'Los Angeles, CA',
+				description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+				url: 'https://www.ziprecruiter.com/jobs',
+				created_at: 'April 5, 2017',
+				dateApplied: 'April 6, 2017',
+				resume: 'http://placehold.it/350x150',
+				coverLetter: 'http://placehold.it/350x150',
+			},
+			{
+				id: 2,
+				company: 'General Assembly',
+				title: 'Instructor',
+				location: 'Santa Monica, CA',
+				description: 'TEACH!',
+				url: 'https://www.generalassemb.ly',
+				created_at: 'March 5, 2017',
+				dateApplied: 'March 6, 2017',
+				resume: 'http://placehold.it/350x150',
+				coverLetter: 'http://placehold.it/350x150',
+			}]
+
+		}
+	},
 	addApp: function() {
-		console.log('addapp')
-		applications.push({
-			id: 3,
-			company: 'ZipRecruiter',
-			title: 'Full-Stack Developer',
-			location: 'Santa Monica, CA',
-			description: 'TEACH!',
-			url: 'https://www.generalassemb.ly',
-			created_at: 'March 5, 2017',
-			dateApplied: 'March 6, 2017',
-			resume: 'http://placehold.it/350x150',
-			coverLetter: 'http://placehold.it/350x150',
-		})
+		setTimeout(() => {
+			this.setState({	
+				applications: [{
+						id: 3,
+						company: 'ZipRecruiter',
+						title: 'Full-Stack Developer',
+						location: 'Santa Monica, CA',
+						description: 'TEACH!',
+						url: 'https://www.generalassemb.ly',
+						created_at: 'March 5, 2017',
+						dateApplied: 'March 6, 2017',
+						resume: 'http://placehold.it/350x150',
+						coverLetter: 'http://placehold.it/350x150',
+					},{
+						id: 1,
+						company: 'Hulu',
+						title: 'Software Developer',
+						location: 'Los Angeles, CA',
+						description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+						url: 'https://www.ziprecruiter.com/jobs',
+						created_at: 'April 5, 2017',
+						dateApplied: 'April 6, 2017',
+						resume: 'http://placehold.it/350x150',
+						coverLetter: 'http://placehold.it/350x150',
+					},
+					{
+						id: 2,
+						company: 'General Assembly',
+						title: 'Instructor',
+						location: 'Santa Monica, CA',
+						description: 'TEACH!',
+						url: 'https://www.generalassemb.ly',
+						created_at: 'March 5, 2017',
+						dateApplied: 'March 6, 2017',
+						resume: 'http://placehold.it/350x150',
+						coverLetter: 'http://placehold.it/350x150',
+					}]
+			})
+		}, 5000)
 	},
 	render: function() {
+		this.addApp()
 
+		var applications = this.state.applications
 		var renderApplications = () => {
 			if (applications.length === 0) {
 				return <p>No applications...</p>
@@ -37,7 +94,7 @@ export var JobApplications = React.createClass({
 		return (
 		<div>
 			<h1 className='page-title'>Applications</h1>
-			<NewApplication onClick={this.addApp}/>
+			<NewApplication/>
 			{renderApplications()}
 		</div>
 		)
@@ -45,29 +102,3 @@ export var JobApplications = React.createClass({
 })
 
 export default JobApplications
-
-
-var applications = [{
-	id: 1,
-	company: 'Hulu',
-	title: 'Software Developer',
-	location: 'Los Angeles, CA',
-	description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-	url: 'https://www.ziprecruiter.com/jobs',
-	created_at: 'April 5, 2017',
-	dateApplied: 'April 6, 2017',
-	resume: 'http://placehold.it/350x150',
-	coverLetter: 'http://placehold.it/350x150',
-},
-{
-	id: 2,
-	company: 'General Assembly',
-	title: 'Instructor',
-	location: 'Santa Monica, CA',
-	description: 'TEACH!',
-	url: 'https://www.generalassemb.ly',
-	created_at: 'March 5, 2017',
-	dateApplied: 'March 6, 2017',
-	resume: 'http://placehold.it/350x150',
-	coverLetter: 'http://placehold.it/350x150',
-}]
